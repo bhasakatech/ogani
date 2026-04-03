500193dcefb889417a81df0579ed5ef2b38fca7f
Shimangali Raj <rajshimangali@gmail.com>
2026-04-03 11:23:59 +0530

    DEV-16 Product Category Carousal

---
+++ core/src/main/java/com/bhasaka/ogani/core/models/CategoryCarouselModel.java
@@ -0,0 +1,60 @@
+package com.bhasaka.ogani.core.models;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import com.bhasaka.ogani.core.models.beans.Category;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(
+        adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class CategoryCarouselModel {
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    @ValueMapValue
+    private String cfPath;
+
+    private List<Category> categories = new ArrayList<>();
+
+    @PostConstruct
+    protected void init() {
+
+        Resource folder = resourceResolver.getResource(cfPath);
+
+        if (folder != null) {
+            Iterator<Resource> fragments = folder.listChildren();
+
+            while (fragments.hasNext()) {
+                Resource cf = fragments.next();
+
+                Resource data = cf.getChild("jcr:content/data/master");
+
+                if (data != null) {
+                    String title = data.getValueMap().get("title", String.class);
+                    String image = data.getValueMap().get("image", String.class);
+                    String link = data.getValueMap().get("link", String.class);
+
+                    if (title != null && image != null) {
+                        categories.add(new Category(title, image, link));
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+    
+}
---
+++ core/src/test/java/com/bhasaka/ogani/core/models/beans/CategoryTest.java
@@ -0,0 +1,39 @@
+package com.bhasaka.ogani.core.models.beans;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void testConstructorAndGetters() {
+        Category category = new Category(
+                "Apple",
+                "/content/dam/apple.png",
+                "/content/apple"
+        );
+
+        assertEquals("Apple", category.getTitle());
+        assertEquals("/content/dam/apple.png", category.getImage());
+        assertEquals("/content/apple", category.getLink());
+    }
+
+    @Test
+    void testNullValues() {
+        Category category = new Category(null, null, null);
+
+        assertNull(category.getTitle());
+        assertNull(category.getImage());
+        assertNull(category.getLink());
+    }
+
+    @Test
+    void testEmptyValues() {
+        Category category = new Category("", "", "");
+
+        assertEquals("", category.getTitle());
+        assertEquals("", category.getImage());
+        assertEquals("", category.getLink());
+    }
+}
