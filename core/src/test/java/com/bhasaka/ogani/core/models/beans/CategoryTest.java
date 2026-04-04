79a696b51d3ab3339473def0a14bd7f5371499f9
Saloni <kumarisaloni2k1@gmail.com>
2026-04-04 12:31:06 +0530

    Fixing the import issue

--- core/src/test/java/com/bhasaka/ogani/core/models/beans/CategoryTest.java
+++ core/src/test/java/com/bhasaka/ogani/core/models/beans/CategoryTest.java
@@ -1,10 +1,13 @@
 package com.bhasaka.ogani.core.models.beans;
 
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static junitx.framework.Assert.assertEquals;
+
 
 @ExtendWith({AemContextExtension.class, MockitoExtension.class})
 class CategoryTest {
