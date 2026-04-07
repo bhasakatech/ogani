744ae65a77955c97f563d2dcf6ac983496d64f6d
RoopaJogu <jogu.roopa28@gmail.com>
2026-04-07 17:20:32 +0530

    Added contact form

---
+++ core/src/main/java/com/bhasaka/ogani/core/config/S3Configuration.java
@@ -0,0 +1,29 @@
+//package com.bhasaka.ogani.core.config;
+
+
+
+package com.bhasaka.ogani.core.config;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(
+        name = "Ogani S3 Configuration"
+)
+public @interface S3Configuration {
+
+    @AttributeDefinition(name = "Access Key")
+    String accessKey();
+
+    @AttributeDefinition(name = "Secret Key")
+    String secretKey();
+
+    @AttributeDefinition(name = "Bucket Name")
+    String bucketName();
+
+    @AttributeDefinition(name = "Region")
+    String region() default "ap-south-1";
+
+    @AttributeDefinition(name = "Base Folder")
+    String baseFolder() default "contact-form";
+}
---
+++ core/src/main/java/com/bhasaka/ogani/core/services/S3UploadService.java
@@ -0,0 +1,5 @@
+package com.bhasaka.ogani.core.services;
+
+public interface S3UploadService {
+    void uploadToS3(String jsonData);
+}
